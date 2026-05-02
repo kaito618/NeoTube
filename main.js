a531dfdfbdebb85b734189f2b6f348656ca0c767
kaito618 <kareemmm618@gmail.com>
2026-05-02 23:04:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("vid").addEventListener("click", function(){
+    let link = this.getAttribute("link");
+    window.open(link, "_blank");
+})
+document.getElementById("vid2").addEventListener("click", function(){
+    let link = this.getAttribute("link");
+    window.open(link, "_blank");
+})
+document.getElementById("vid3").addEventListener("click", function(){
+    let link = this.getAttribute("link");
+    window.open(link, "_blank");
+})
+document.getElementById("page-swapper").addEventListener("click", function(){
+    window.location.href = "page2.html";
+})
